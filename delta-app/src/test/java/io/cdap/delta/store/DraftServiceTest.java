9f891711547838fc7bb87d14f93ae4ce1ac10e70
Shifu Xu <shifuxu@google.com>
2020-02-13 14:09:23 -0800

    Remove unused imports.

--- delta-app/src/test/java/io/cdap/delta/store/DraftServiceTest.java
+++ delta-app/src/test/java/io/cdap/delta/store/DraftServiceTest.java
@@ -55,7 +55,6 @@ import java.sql.JDBCType;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
