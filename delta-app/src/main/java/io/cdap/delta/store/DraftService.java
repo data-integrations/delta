ffa71701eb040696d47aa98683705e0e52a4e527
Shifu Xu <58008032+shifuxu0301@users.noreply.github.com>
2020-03-31 10:32:10 -0700

    CDAP-16475 Add feature problem for unselected primary key during assessment. (#47)
    * Add feature problem for unselected primary key during assessment.
    
    * Add problem based on how many primary key unselected.

--- delta-app/src/main/java/io/cdap/delta/store/DraftService.java
+++ delta-app/src/main/java/io/cdap/delta/store/DraftService.java
@@ -290,6 +290,30 @@ public class DraftService {
 
     // fetch detail about the table, then filter out columns that will not be read by the source
     TableDetail detail = tableRegistry.describeTable(db, table);
+    List<Problem> missingFeatures = new ArrayList<>(detail.getFeatures());
+    List<String> unselectedPrimaryKeys = detail.getPrimaryKey().stream()
+      // if there are no columns specified, it means this primary key will be read/selected
+      .filter(columnWhitelist.isEmpty() ? col -> false : col -> !columnWhitelist.contains(col))
+      .collect(Collectors.toList());
+    if (unselectedPrimaryKeys.size() == 1) {
+      missingFeatures.add(
+        new Problem("Missing Primary Key",
+                    String.format("Column '%s' is part of the primary key for table '%s' in database '%s', " +
+                                    "but is not selected to be replicated", unselectedPrimaryKeys.get(0),
+                                  detail.getTable(), detail.getDatabase()),
+                    "Please make sure this column been selected",
+                    "This can result in different data at the target than at the source"));
+    } else if (unselectedPrimaryKeys.size() > 1) {
+      missingFeatures.add(
+        new Problem("Missing Primary Key",
+                    String.format("Columns '%s' are part of the primary key for table '%s' in database '%s', " +
+                                    "but are not selected to be replicated",
+                                  String.join(",", unselectedPrimaryKeys),
+                                  detail.getTable(), detail.getDatabase()),
+                    "Please make sure columns been selected",
+                    "This can result in different data at the target than at the source"));
+    }
+
     List<ColumnDetail> selectedColumns = detail.getColumns().stream()
       // if there are no columns specified, it means all columns should be read
       .filter(columnWhitelist.isEmpty() ? col -> true : col -> columnWhitelist.contains(col.getName()))
@@ -297,7 +321,7 @@ public class DraftService {
     TableDetail filteredDetail = TableDetail.builder(db, table, detail.getSchema())
       .setPrimaryKey(detail.getPrimaryKey())
       .setColumns(selectedColumns)
-      .setFeatures(detail.getFeatures())
+      .setFeatures(missingFeatures)
       .build();
     TableAssessment srcAssessment = sourceTableAssessor.assess(filteredDetail);
 
