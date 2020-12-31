5551c76da062f406e4c01d660ac0ab40bbab6d65
sagarkapare <sagarkapare@google.com>
2020-12-30 16:07:15 -0800

    test logs

--- delta-app/src/main/java/io/cdap/delta/app/QueueingEventEmitter.java
+++ delta-app/src/main/java/io/cdap/delta/app/QueueingEventEmitter.java
@@ -26,6 +26,8 @@ import io.cdap.delta.api.EventReaderDefinition;
 import io.cdap.delta.api.Sequenced;
 import io.cdap.delta.api.SourceTable;
 import io.cdap.delta.proto.DBTable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Set;
@@ -39,6 +41,7 @@ import java.util.stream.Collectors;
  * If emitting an event is interrupted, all subsequent emit calls will be no-ops.
  */
 public class QueueingEventEmitter implements EventEmitter {
+  private static final Logger LOG = LoggerFactory.getLogger(QueueingEventEmitter.class);
   private final Set<DMLOperation.Type> dmlBlacklist;
   private final Set<DDLOperation.Type> ddlBlacklist;
   private final Map<DBTable, SourceTable> tableDefinitions;
@@ -82,6 +85,9 @@ public class QueueingEventEmitter implements EventEmitter {
     try {
       eventQueue.put(new Sequenced<>(event, event.getSourceTimestampMillis() == null ? ++sequenceNumber
         : event.getSourceTimestampMillis()));
+      if (event.getOperation().getType() == DMLOperation.Type.INSERT) {
+        LOG.info("Op: I, Seq: {}, Id: {}", sequenceNumber, event.getRow().get("id"));
+      }
     } catch (InterruptedException e) {
       // this should only happen when the event consumer is stopped
       // in that case, don't emit any more events
