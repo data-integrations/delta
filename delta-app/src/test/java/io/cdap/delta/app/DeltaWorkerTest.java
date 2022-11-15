915b0e929a324395c006be9b76dc1291b7b9781c
yjhawar <111336680+yjhawar@users.noreply.github.com>
2022-11-15 15:39:34 +0530

    Throw DeltaFailureRuntimeException when pipeline is run without prima…(CDAP-19607)  (#193)
    * Throw DeltaFailureRuntimeException when pipeline is run without primary key in table.
    
    * changed year
    
    * Added unit test for testing DeltaFailure Runtime exception.
    
    * resolved comments.

--- delta-app/src/test/java/io/cdap/delta/app/DeltaWorkerTest.java
+++ delta-app/src/test/java/io/cdap/delta/app/DeltaWorkerTest.java
@@ -16,8 +16,17 @@
 
 package io.cdap.delta.app;
 
+import io.cdap.cdap.api.app.ApplicationSpecification;
+import io.cdap.cdap.api.worker.WorkerConfigurer;
+import io.cdap.cdap.api.worker.WorkerContext;
+import io.cdap.cdap.api.worker.WorkerSpecification;
+import io.cdap.cdap.app.DefaultAppConfigurer;
+import io.cdap.cdap.internal.app.DefaultApplicationSpecification;
+import io.cdap.delta.api.DeltaFailureRuntimeException;
 import io.cdap.delta.api.DeltaSource;
 import io.cdap.delta.api.DeltaTarget;
+import io.cdap.delta.api.EventConsumer;
+import io.cdap.delta.api.EventReader;
 import io.cdap.delta.api.SourceTable;
 import io.cdap.delta.proto.Artifact;
 import io.cdap.delta.proto.DeltaConfig;
@@ -26,20 +35,40 @@ import io.cdap.delta.proto.ParallelismConfig;
 import io.cdap.delta.proto.Plugin;
 import io.cdap.delta.proto.Stage;
 import io.cdap.delta.proto.TableId;
+import io.cdap.delta.store.RemoteStateStore;
+import io.cdap.delta.store.StateStoreMigrator;
+import org.apache.twill.api.RunId;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 
+import static org.mockito.Mockito.*;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Tests for {@link DeltaWorker}.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({WorkerSpecification.class, DefaultApplicationSpecification.class, DeltaWorker.class})
+
 public class DeltaWorkerTest {
+  @Rule
+  final ExpectedException exception = ExpectedException.none();
   private static final SourceTable TABLE1 = new SourceTable("db", "t1");
   private static final SourceTable TABLE2 = new SourceTable("db", "t2");
   private static final SourceTable TABLE3 = new SourceTable("db", "t3");
@@ -100,5 +129,57 @@ public class DeltaWorkerTest {
     expected.put(1, instance1Tables);
     Map<Integer, Set<TableId>> assignments = DeltaWorker.assignTables(config);
     Assert.assertEquals(expected, assignments);
+  }
+
+  @Test
+  public void testDeltaFailureRuntimeException() throws Exception {
+    //Mock delta config
+    DeltaConfig config = mock(DeltaConfig.class);
+    when(config.getParallelism()).thenReturn(mock(ParallelismConfig.class));
+    //Mock worker context,application specification,event reader and event consumer for initialize method
+    WorkerContext workerContext = mock(WorkerContext.class);
+    when(workerContext.getNamespace()).thenReturn("default");
+    when(workerContext.getInstanceId()).thenReturn(0);
+    when(workerContext.getRunId()).thenReturn(mock(RunId.class));
+    DeltaSource deltaSource = mock(DeltaSource.class);
+    DeltaTarget deltaTarget = mock(DeltaTarget.class);
+    EventReader eventReader = mock(EventReader.class);
+    EventConsumer eventConsumer = mock(EventConsumer.class);
+    doNothing().when(eventReader).start(any());
+    doNothing().when(eventConsumer).start();
+    when(deltaSource.createReader(any(), any(), any())).thenReturn(eventReader);
+    when(deltaTarget.createConsumer(any())).thenReturn(eventConsumer);
+    when(workerContext.newPluginInstance(eq("Microsoft SQLServer"), any())).thenReturn(deltaSource);
+    when(workerContext.newPluginInstance(eq("BigQuery"), any())).thenReturn(deltaTarget);
+    ApplicationSpecification appSpec = PowerMockito.mock(DefaultApplicationSpecification.class);
+    when(appSpec.getName()).thenReturn("DeltaWorker");
+    when(workerContext.getApplicationSpecification()).thenReturn(appSpec);
+    when(appSpec.getConfiguration()).thenReturn(
+      new String(Files.readAllBytes(Paths.get("./src/test/java/io/cdap/delta/app/appSpec-config.json"))));
+
+    WorkerSpecification spec = PowerMockito.mock(WorkerSpecification.class);
+    PowerMockito.when(workerContext.getSpecification()).thenReturn(spec);
+    PowerMockito.when(spec.getProperty("generation")).thenReturn("0");
+    PowerMockito.when(spec.getProperty("table.assignments"))
+      .thenReturn("{\"0\":[{\"database\":\"testreplication\",\"table\":\"npe\",\"schema\":\"dbo\"}]}");
+
+    RemoteStateStore remoteStateStore = PowerMockito.mock(RemoteStateStore.class);
+    PowerMockito.whenNew(RemoteStateStore.class).withArguments(workerContext).thenReturn(remoteStateStore);
+    when(remoteStateStore.readState(any(), any())).thenReturn(null);
+
+    StateStoreMigrator stateStoreMigrator = PowerMockito.mock(StateStoreMigrator.class);
+    PowerMockito.whenNew(StateStoreMigrator.class).withArguments(any(), any(), any()).thenReturn(stateStoreMigrator);
+    when(stateStoreMigrator.perform()).thenReturn(false);
+
+    //Throw Delta Failure runtime exception while polling event queue in run method
+    CapacityBoundedEventQueue eventQueue = mock(CapacityBoundedEventQueue.class);
+    PowerMockito.whenNew(CapacityBoundedEventQueue.class).withArguments(anyInt(), anyLong()).thenReturn(eventQueue);
+    doThrow(new DeltaFailureRuntimeException("")).when(eventQueue).poll(anyLong(), eq(TimeUnit.SECONDS));
+
+    DeltaWorker deltaWorker = new DeltaWorker(config, null, mock(DefaultAppConfigurer.class));
+    deltaWorker.configure(mock(WorkerConfigurer.class));
+    deltaWorker.initialize(workerContext);
+    exception.expect(DeltaFailureRuntimeException.class);
+    deltaWorker.run();
   }
 }
