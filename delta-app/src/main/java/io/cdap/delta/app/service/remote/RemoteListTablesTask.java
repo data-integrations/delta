25e65d3835342a9dff19b131da5a3a576d5978d5
sumitjnn <108464238+sumitjnn@users.noreply.github.com>
2022-12-22 09:45:38 +0530

    CDAP-20113 - Offload plugin execution to Task Workers in security enabled configurations (#199)
    * CDAP-20113 - Offload plugin execution to Task Workers in security enabled configurations
    
    * Fixed formatting issues
    
    * Add unit test for AssessmentHandler
    
    * Fix review comments for CDAP-20113

---
+++ delta-app/src/main/java/io/cdap/delta/app/service/remote/RemoteListTablesTask.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.delta.app.service.remote;
+
+import io.cdap.cdap.api.service.worker.RunnableTask;
+import io.cdap.cdap.api.service.worker.SystemAppTaskContext;
+import io.cdap.delta.api.Configurer;
+import io.cdap.delta.api.assessment.TableList;
+import io.cdap.delta.proto.DeltaConfig;
+
+/**
+ * {@link RunnableTask} for executing list tables remotely
+ */
+public class RemoteListTablesTask extends RemoteAssessmentTaskBase {
+
+  @Override
+  public String execute(SystemAppTaskContext systemAppContext,
+                        RemoteAssessmentRequest request) throws Exception {
+    String namespace = request.getNamespace();
+    DeltaConfig deltaConfig = request.getConfig();
+    Configurer configurer = getConfigurer(systemAppContext, namespace);
+
+    TableList tableList = getAssessor().listTables(namespace, deltaConfig, configurer);
+    return GSON.toJson(tableList);
+  }
+}
